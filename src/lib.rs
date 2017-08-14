4dbbfb12d028f4f36772e3a9f9f3ba6a50a05d6c
Martin Tomasi <martin.tomasi@gmail.com>
2017-08-14 23:51:24 +0200

    added some docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
+//! An asynchronous imgur API wrapper using `hyper` 0.11 and `serde`.
 #![feature(conservative_impl_trait)]
+#![deny(missing_debug_implementations, missing_copy_implementations, trivial_casts,
+       trivial_numeric_casts, unsafe_code, unused_import_braces, unused_qualifications)]
+#![warn(missing_docs)]
+#![cfg_attr(feature = "clippy", feature(plugin))]
+#![cfg_attr(feature = "clippy", plugin(clippy))]
+#![recursion_limit = "128"]
 
 extern crate hyper;
 extern crate futures;
@@ -11,7 +18,7 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
-#[macro_use]
+// #[macro_use]
 extern crate log;
 extern crate env_logger;
 
@@ -26,7 +33,7 @@ mod errors {
             Tls(native_tls::Error);
             Hyper(hyper::Error);
             Serde(serde_json::Error);
-            Imgur(super::ImgurError);
+            Imgur(super::ApiError);
         }
     }
 }
@@ -41,30 +48,34 @@ use tokio_core::reactor::Handle;
 use futures::{future, Future, Stream};
 use serde::de::DeserializeOwned;
 
-use errors::{Error, Result};
+pub use errors::{Error, Result};
 
 const DEFAULT_THREADS: usize = 2;
 const API: &str = "https://api.imgur.com/3";
 
-pub type HttpsClient = Client<HttpsConnector<HttpConnector>>;
+type HttpsClient = Client<HttpsConnector<HttpConnector>>;
 
+/// Main client type.
+#[derive(Debug, Clone)]
 pub struct ImgurClient {
     client: HttpsClient,
     client_id: String,
 }
 
 impl ImgurClient {
+    /// Create a new `ImgurClient`.
     pub fn new(handle: &Handle, client_id: String) -> Result<ImgurClient> {
         let connector = HttpsConnector::new(DEFAULT_THREADS, handle)?;
         let client = Client::configure().connector(connector).build(handle);
         Ok(ImgurClient { client, client_id })
     }
 
+    ///  Create a new `ImgurClient` with a supplied `hyper::Client`.
     pub fn with_client(client: HttpsClient, client_id: String) -> ImgurClient {
         ImgurClient { client, client_id }
     }
 
-    fn get_with_auth<T>(&self, url: Uri) -> impl Future<Item = T, Error = Error>
+    fn get_with_header<T>(&self, url: Uri) -> impl Future<Item = T, Error = Error>
     where
         T: DeserializeOwned,
     {
@@ -81,64 +92,87 @@ impl ImgurClient {
         )
     }
 
-    pub fn image(&self, id: &str) -> impl Future<Item = ImgurResponse<Image>, Error = Error> {
+    /// Gets data for an image (`GET /image/<id>`)
+    pub fn image(&self, id: &str) -> impl Future<Item = Response<Image>, Error = Error> {
         let url = format!("{}/image/{}", API, id).parse().unwrap();
-        self.get_with_auth(url)
+        self.get_with_header(url)
     }
 
+    /// Gets data for all the images in an album. (`GET /album/<album_id>/images`).
     pub fn album_images(
         &self,
         album_id: &str,
-    ) -> impl Future<Item = ImgurResponse<Vec<Image>>, Error = Error> {
+    ) -> impl Future<Item = Response<Vec<Image>>, Error = Error> {
         let url = format!("{}/album/{}/images", API, album_id)
             .parse()
             .unwrap();
-        self.get_with_auth(url)
+        self.get_with_header(url)
     }
 }
 
+/// Wrapper type returned from all the web API methods.
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ImgurResponse<T> {
+pub struct Response<T> {
+    /// HTTP status of the response.
     pub status: usize,
+    /// Whether the response succeeded.
     pub success: bool,
-    pub data: ImgurData<T>,
+    /// Either data returned from the API or an error (see `ResponseData`)
+    pub data: ResponseData<T>,
 }
 
+/// The `data` field of the JSON response can either be some data (e.g. data for an image)
+/// or an error.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
-pub enum ImgurData<T> {
+pub enum ResponseData<T> {
+    /// Successful response.
     Success(T),
-    Error(ImgurError),
+    /// Error response.
+    Error(ApiError),
 }
 
-impl<T> ImgurData<T> {
+impl<T> ResponseData<T> {
+    /// Converts `self` into a `Result`.
     pub fn into_result(self) -> Result<T> {
         match self {
-            ImgurData::Success(v) => Ok(v),
-            ImgurData::Error(e) => Err(e.into()),
+            ResponseData::Success(v) => Ok(v),
+            ResponseData::Error(e) => Err(e.into()),
         }
     }
 }
 
+/// Error data returned by the API.
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ImgurError {
+pub struct ApiError {
+    /// Error description.
     pub error: String,
+    /// Request URL.
     pub request: String,
+    /// HTTP method used for the request.
     pub method: String,
 }
 
-impl fmt::Display for ImgurError {
+impl fmt::Display for ApiError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Request {} {} failed: {}", self.method, self.request, self.error)
+        write!(
+            f,
+            "Request {} {} failed: {}",
+            self.method,
+            self.request,
+            self.error
+        )
     }
 }
 
-impl error::Error for ImgurError {
+impl error::Error for ApiError {
     fn description(&self) -> &str {
         &self.error
     }
 }
 
+/// Data returned for an image.
+#[allow(missing_docs)]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Image {
     pub account_id: Option<String>,
